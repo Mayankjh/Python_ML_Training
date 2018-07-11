d179a19bc2a0ce0fe67eaf60e9eba5d7c2f087f5
Mayank Jha <mayankjha7722@gmail.com>
2018-07-11 09:41:44 +0530

    Taks done!!

--- Python Training/Day 6/Namecheck.py
+++ Python Training/Day 6/Namecheck.py
@@ -1,17 +1,21 @@
 def main():
     try:
         name = ['Mayank','Karan','Kritarth','Geetansh','Piyush']
+        op = []
 #         for line in readfile('lines.txt'):
 #             print(line.strip())
         for names in readfile('names.txt'):
            outfile = open('new.txt','w')
            data = names.strip()
            if data not in name:
-               outfile.write(data +"\n")
+               #outfile.write(data +"\n")
+               op.append(data)
                print("done...")
            else:
               pass
-
+        for names in op:
+            outfile.write(names +"\n")
+            
     except IOError as e:
         print('cannot read file',e)
     except ValueError as e:
